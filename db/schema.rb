5875547bc9eb4c2c8dfa9aea326b2cb022bcfbf8
zep1994 <snaxbags@protonmail.com>
2018-11-09 15:14:27 -0600

    Generate Students Resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_09_201131) do
+ActiveRecord::Schema.define(version: 2018_11_09_210833) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_number"
+    t.string "email"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_students_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
